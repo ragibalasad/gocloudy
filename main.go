754db0692cdf065939e4ba1fe083513851614ba2
Ragib Al Asad <ragibalasad@gmail.com>
2024-10-01 17:16:42 +0600

    Get json response from weather api

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("ready to go")
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=1a79fe38d99243d89ac92056240110&q=Rangpur&days=1&aqi=no&alerts=no")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather API not available")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println(string(body))
 }
